54378b9f7c1292ec2667a93cde14b500012abfc1
HyukWoo Park <hyukwoo.park@samsung.com>
2024-03-18 19:38:52 +0900

    Add TC for minor parsing bugs
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

---
+++ Escargot/regression-tests/xfail/issue-1323.js
@@ -0,0 +1,8 @@
+var r = new Uint8Array ( 1 ) ;
+class n extends Int8Array {
+    constructor ( r ) {
+        super ( r ) ;
+        eval ( " super ( ) ; " ) ;
+    }
+}
+r. constructor = n, r. map ( function ( ) { } ) ;
