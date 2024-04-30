fb9e23e5c1bf9320d23a1093d14ed7efc1415ee1
HyukWoo Park <hyukwoo.park@samsung.com>
2024-04-30 11:25:35 +0900

    Add TC where lexical block is added together with continue/break statement
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

---
+++ Escargot/regression-tests/issue-1336.js
@@ -0,0 +1,55 @@
+(function func0 () {
+    for (let var0 in this) {
+        class e { }
+        if (- " number " in this) continue ;
+    }
+    with ( 0 ) { }
+})();
+
+(function func0 () {
+    for (let var0 in this) {
+        class e { }
+        if (- " number " in this) break ;
+    }
+    with ( 0 ) { }
+})();
+
+(function func0 () {
+    let var0 = 10;
+    while (var0 > 0) {
+        var0--;
+        class e { }
+        if (- " number " in this) continue ;
+    }
+    with ( 0 ) { }
+})();
+
+(function func0 () {
+    let var0 = 10;
+    while (var0 > 0) {
+        var0--;
+        class e { }
+        if (- " number " in this) break ;
+    }
+    with ( 0 ) { }
+})();
+
+(function func0 () {
+    let var0 = 10;
+    do {
+        var0--;
+        class e { }
+        if (- " number " in this) continue ;
+    } while (var0 > 0)
+    with ( 0 ) { }
+})();
+
+(function func0 () {
+    let var0 = 10;
+    do {
+        var0--;
+        class e { }
+        if (- " number " in this) break ;
+    } while (var0 > 0)
+    with ( 0 ) { }
+})();
