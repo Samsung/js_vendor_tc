339e90d35d45b03485c9d9eb5dcfb72511d8263d
HyukWoo Park <hyukwoo.park@samsung.com>
2024-07-04 15:27:16 +0900

    Add a TC to test recursive Array.toString calls
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

---
+++ Escargot/new-es/array-toString.js
@@ -0,0 +1,32 @@
+/* Copyright 2024-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function F0(a2, a3) {
+}
+
+var v4 = new F0();
+
+function f5(a6, a7) {
+    var o14 = {
+        toString(a9, a10, a11) {
+            var v12 = [a11,this];
+            v12 - v12;
+        },
+    };
+
+    try { o14.toString(); } catch (e) {}
+}
+
+f5(f5, v4);
