40a372e4d26cea0fec929c83c23f25b0840ad9fe
Seonghyun Kim <sh8281.kim@samsung.com>
2025-06-26 11:20:16 +0900

    Update #1379 case due to spec update
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

--- Escargot/regression-tests/issue-1379.js
+++ Escargot/regression-tests/issue-1379.js
@@ -1,3 +1,8 @@
-const v0 = /(?<v>>||||`|^||||||||||(?<c>(?<d>.)?).)+(?<c>(?<d>.D*))\b/dsyig;
-v0.kwstIndex = "2";
-v0.test("nud");
+var err;
+try {
+    eval("const v0 = /(?<v>>||||`|^||||||||||(?<c>(?<d>.)?).)+(?<c>(?<d>.D*))\b/dsyig; v0.kwstIndex = '2'; v0.test('nud;');");
+} catch (e) {
+err =e;
+}
+
+assert(err instanceof SyntaxError);
